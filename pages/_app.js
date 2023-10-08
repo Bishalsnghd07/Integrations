fde5c66d00af4ff2fc85f6a344169ee07f829a2a
Bishal Singh Deo <88236153+Bishalsnghd07@users.noreply.github.com>
2023-10-08 19:26:35 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,19 @@
-import { Fragment } from "react";
-import "./global.css";
+// import { Fragment } from "react";
+// import "./global.css";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Fragment>
-      <Head>
-        <title>Demo</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </Fragment>
-  );
-}
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <Fragment>
+//       <Head>
+//         <title>Demo</title>
+//         <meta
+//           name="viewport"
+//           content="minimum-scale=1, initial-scale=1, width=device-width"
+//         />
+//       </Head>
+//       <Component {...pageProps} />
+//     </Fragment>
+//   );
+// }
 
-export default MyApp;
+// export default MyApp;
