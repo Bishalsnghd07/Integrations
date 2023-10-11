7a72bc5102ae883d35b77599b2046d2c7619e03a
Bishal Singh Deo <88236153+Bishalsnghd07@users.noreply.github.com>
2023-10-11 17:53:05 +0530

    Update jira-card.js

--- components/jira-card.js
+++ components/jira-card.js
@@ -2,18 +2,17 @@ const JiraCard = () => {
     return (
       <div className="absolute top-[80.19rem] left-[31.25rem] w-[75.06rem] h-[6.31rem] opacity-[0.5] text-left text-[1.13rem] text-heading font-poppins">
         <div className="absolute top-[0rem] left-[0rem] w-[75.06rem] h-[6.31rem]">
-        <div className="absolute top-[6.3rem] left-[75.06rem] rounded-3xl [background:linear-gradient(90.11deg,_rgba(255,_255,_255,_0),_rgba(239,_239,_239,_0.5))] box-border w-[75.06rem] h-[6.31rem] [transform:_rotate(180deg)] [transform-origin:0_0] border-[1px] border-solid border-whitesmoke-200" />
-          <div className="absolute top-[-1.25rem] left-[75.06rem] rounded-3xl [background:linear-gradient(90.11deg,_rgba(255,_255,_255,_0),_rgba(239,_239,_239,_0.5))] box-border w-[75.06rem] h-[6.31rem] [transform:_rotate(180deg)] [transform-origin:0_0] border-[1px] border-solid border-whitesmoke-200" />
+        <div className="absolute top-[135rem] sm:top-[6.3rem] left-[55.80rem] sm:left-[75.06rem] rounded-3xl bg-slate-200  sm:bg-slate-50 box-border  w-[34.06rem] sm:w-[75.06rem] h-[58.31rem] sm:h-[6.31rem] [transform:_rotate(180deg)] [transform-origin:0_0] border-[1px] border-solid border-whitesmoke-200" />
+          <div className="absolute top-[135rem] sm:top-[-1.25rem] left-[20.40rem] sm:left-[75.06rem] rounded-3xl bg-slate-200  sm:bg-slate-50 box-border w-[34.06rem] sm:w-[75.06rem] h-[58.31rem] sm:h-[6.31rem] [transform:_rotate(180deg)] [transform-origin:0_0] border-[1px] border-solid border-whitesmoke-200" />
         </div>
-        <div className="absolute top-[1.56rem] left-[5.44rem] font-medium">
+        <div className="absolute top-[78rem] sm:top-[1.56rem] left-[28.60rem] sm:left-[5.44rem] text-[2.13rem] sm:text-[1.13rem] font-medium">
           Jira
         </div>
-        <div className="absolute top-[3.31rem] left-[5.44rem] text-[1rem] text-b778c">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
+        <div className="absolute top-[81.40rem] sm:top-[3.31rem] left-[28.70rem] sm:left-[5.44rem] text-xl sm:text-base text-b778c">
+          Lorem Ipsum is dy text
         </div>
         <img
-          className="absolute top-[1.56rem] left-[1.25rem] rounded-3xs w-[3.25rem] h-[3.25rem] object-cover"
+          className="absolute top-[78rem] sm:top-[1.56rem] left-[24rem] sm:left-[1.25rem] rounded-3xs w-[3.25rem] h-[3.25rem] object-cover"
           alt=""
           src="/image-17@2x.png"
         />
